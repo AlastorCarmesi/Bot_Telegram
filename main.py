978dc9e32e46af1d739eee4a6a9de10a8e344cd4
Alastor_Carmesi <carmesiblog7@gmail.com>
2026-08-11 14:09:27 -0600

    Modificaciones enMain.py

--- main.py
+++ main.py
@@ -2,15 +2,22 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+TK = "8626115500:AAEoxJrhlmjXAnGAuA3PzMMB5c8rHdPGOOQ"
+
+#Metodos para el bot
 async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello World!, bot iniciado correctamente")
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(update.message.text)
 
-application = ApplicationBuilder().token("8626115500:AAEoxJrhlmjXAnGAuA3PzMMB5c8rHdPGOOQ").build()
+async def buscar(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(context.args)
+
+application = ApplicationBuilder().token(TK).build()
 
 application.add_handler( CommandHandler( "start", say_hello ) )
 application.add_handler( CommandHandler( "echo", echo ) )
+application.add_handler( CommandHandler( "help", say_hello ) )
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
