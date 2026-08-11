9927909a0608eb25602a0f981c5f2ebb3455c42b
Alastor_Carmesi <carmesiblog7@gmail.com>
2026-08-11 09:13:30 -0600

    Comando echo

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def say_hello(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Hello World!, bot iniciado correctamente")
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
 application = ApplicationBuilder().token("8626115500:AAEoxJrhlmjXAnGAuA3PzMMB5c8rHdPGOOQ").build()
 
 application.add_handler( CommandHandler( "start", say_hello ) )
+application.add_handler( CommandHandler( "echo", echo ) )
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
